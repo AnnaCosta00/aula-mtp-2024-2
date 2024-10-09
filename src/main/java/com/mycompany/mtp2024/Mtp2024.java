e11549229e605739f9fb08c7c60d9d9386fbbe85
Anna Beatriz Costa Cruz <b.cruz@academico.ifg.edu.br>
2024-10-09 09:47:44 -0300

    commit inicial

---
+++ src/main/java/com/mycompany/mtp2024/Mtp2024.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mtp2024;
+
+/**
+ *
+ * @author 20241070130002
+ */
+public class Mtp2024 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
